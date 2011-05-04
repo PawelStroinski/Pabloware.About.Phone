03f74b9f72562d3a9688bf119b1ecb2b00eb32cb
Pawel (laptop) <pol84@live.com>
2011-05-04 14:29:41 +0200

    zmiana wałbrzych na warszawka

--- Dietphone/Views/ProductEditing.xaml.cs
+++ Dietphone/Views/ProductEditing.xaml.cs
@@ -29,7 +29,7 @@ namespace Dietphone.Views
             var navigator = new NavigatorImpl(NavigationService, NavigationContext);
             viewModel = new ProductEditingViewModel(App.Factories, navigator);
             DataContext = viewModel;
-            MessageBox.Show("Wałbrzych!");
+            MessageBox.Show("Warszawka!");
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
