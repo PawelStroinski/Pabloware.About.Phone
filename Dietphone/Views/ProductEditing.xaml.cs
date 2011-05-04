e9722cdba2b9ddc12c13b31232677369d0d44d9d
Pawel <pol84@live.com>
2011-05-04 14:26:17 +0200

    zmiana venice na wałbrzych

--- Dietphone/Views/ProductEditing.xaml.cs
+++ Dietphone/Views/ProductEditing.xaml.cs
@@ -29,7 +29,7 @@ namespace Dietphone.Views
             var navigator = new NavigatorImpl(NavigationService, NavigationContext);
             viewModel = new ProductEditingViewModel(App.Factories, navigator);
             DataContext = viewModel;
-            MessageBox.Show("Venice!");
+            MessageBox.Show("Wałbrzych!");
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
