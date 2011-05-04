33884b75dbde2148d650b21b2270534ea980b0e5
Pawel <pol84@live.com>
2011-05-04 14:15:49 +0200

    zmiana świnoujścia na tarnobrzeg

--- Dietphone/Views/ProductEditing.xaml.cs
+++ Dietphone/Views/ProductEditing.xaml.cs
@@ -29,7 +29,7 @@ namespace Dietphone.Views
             var navigator = new NavigatorImpl(NavigationService, NavigationContext);
             viewModel = new ProductEditingViewModel(App.Factories, navigator);
             DataContext = viewModel;
-            MessageBox.Show("Świnoujście!");
+            MessageBox.Show("Tarnobrzeg!");
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
