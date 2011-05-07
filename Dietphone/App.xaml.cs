879976649f58de1b939f5dcc495d96298ee21ab5
Pawel <pol84@live.com>
2011-05-07 02:38:34 +0200

    dodany RadPhoneApplicationFrame (animacje)

--- Dietphone/App.xaml.cs
+++ Dietphone/App.xaml.cs
@@ -16,11 +16,14 @@ using Dietphone.ViewModels;
 using Dietphone.Models;
 using Dietphone.BinarySerializers;
 using Microsoft.Phone.Tasks;
+using Telerik.Windows.Controls;
 
 namespace Dietphone
 {
     public partial class App : Application
     {
+        public RadPhoneApplicationFrame RootFrame { get; private set; }
+        private bool phoneApplicationInitialized = false;
         private static Factories factories = null;
         private static readonly object factoriesLock = new object();
 
@@ -47,37 +50,21 @@ namespace Dietphone
             }
         }
 
-        /// <summary>
-        /// Provides easy access to the root frame of the Phone Application.
-        /// </summary>
-        /// <returns>The root frame of the Phone Application.</returns>
-        public PhoneApplicationFrame RootFrame { get; private set; }
-
-        /// <summary>
-        /// Constructor for the Application object.
-        /// </summary>
         public App()
         {
-            // Global handler for uncaught exceptions. 
             UnhandledException += Application_UnhandledException;
-
-            // Show graphics profiling information while debugging.
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 // Display the current frame rate counters
                 //Application.Current.Host.Settings.EnableFrameRateCounter = true;
-
                 // Show the areas of the app that are being redrawn in each frame.
                 //Application.Current.Host.Settings.EnableRedrawRegions = true;
-
                 // Enable non-production analysis visualization mode, 
                 // which shows areas of a page that are being GPU accelerated with a colored overlay.
                 //Application.Current.Host.Settings.EnableCacheVisualization = true;
             }
-
             // Standard Silverlight initialization
             InitializeComponent();
-
             // Phone-specific initialization
             InitializePhoneApplication();
         }
@@ -92,7 +79,6 @@ namespace Dietphone
         // This code will not execute when the application is first launched
         private void Application_Activated(object sender, ActivatedEventArgs e)
         {
-            // Ensure that application state is restored appropriately
         }
 
         // Code to execute when the application is deactivated (sent to background)
@@ -105,46 +91,6 @@ namespace Dietphone
         // This code will not execute when the application is deactivated
         private void Application_Closing(object sender, ClosingEventArgs e)
         {
-            // Ensure that required application state is persisted here.
-        }
-
-        // Code to execute if a navigation fails
-        private void RootFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                // A navigation has failed; break into the debugger
-                System.Diagnostics.Debugger.Break();
-            }
-        }
-
-        // Code to execute on Unhandled Exceptions
-        private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
-        {
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                // An unhandled exception has occurred; break into the debugger
-                System.Diagnostics.Debugger.Break();
-            }
-            else
-            {
-                var exception = e.ExceptionObject;
-                MailException(exception.ToString());
-                e.Handled = true;
-            }
-        }
-
-        private void MailException(string exception)
-        {
-            if (MessageBox.Show(exception, "A niech to, pluskwa w aplikacji! Zgłosić autorowi?",
-                MessageBoxButton.OKCancel) == MessageBoxResult.OK)
-            {
-                EmailComposeTask task = new EmailComposeTask();
-                task.To = "pol84@live.com";
-                task.Body = String.Format("Hej! Zgłaszam następującą pluskwę:\r\n\r\n{0}", exception);
-                task.Subject = "Pluskwa!";
-                task.Show();
-            }
         }
 
         private static void CreateFactories()
@@ -153,40 +99,63 @@ namespace Dietphone
             factories = new FactoriesImpl(binary);
         }
 
-        #region Phone application initialization
-
-        // Avoid double-initialization
-        private bool phoneApplicationInitialized = false;
-
-        // Do not add any additional code to this method
         private void InitializePhoneApplication()
         {
             if (phoneApplicationInitialized)
                 return;
-
             // Create the frame but don't set it as RootVisual yet; this allows the splash
             // screen to remain active until the application is ready to render.
-            RootFrame = new PhoneApplicationFrame();
+            RootFrame = new RadPhoneApplicationFrame();
             RootFrame.Navigated += CompleteInitializePhoneApplication;
-
-            // Handle navigation failures
             RootFrame.NavigationFailed += RootFrame_NavigationFailed;
-
-            // Ensure we don't initialize again
             phoneApplicationInitialized = true;
         }
 
-        // Do not add any additional code to this method
         private void CompleteInitializePhoneApplication(object sender, NavigationEventArgs e)
         {
-            // Set the root visual to allow the application to render
             if (RootVisual != RootFrame)
                 RootVisual = RootFrame;
-
-            // Remove this handler since it is no longer needed
             RootFrame.Navigated -= CompleteInitializePhoneApplication;
         }
 
-        #endregion
+        private void RootFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+            else
+            {
+                var exception = e.ExceptionObject;
+                SendExceptionQuestion(exception.ToString());
+                e.Handled = true;
+            }
+        }
+
+        private void SendExceptionQuestion(string exception)
+        {
+            if (MessageBox.Show(exception, "A niech to, pluskwa w aplikacji! Zgłosić autorowi?",
+                MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+            {
+                SendException(exception);
+            }
+        }
+
+        private void SendException(string exception)
+        {
+            EmailComposeTask task = new EmailComposeTask();
+            task.To = "pol84@live.com";
+            task.Body = String.Format("Hej! Zgłaszam następującą pluskwę:\r\n\r\n{0}", exception);
+            task.Subject = "Pluskwa!";
+            task.Show();
+        }
     }
 }
