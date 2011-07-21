a842bfde33b420d360d211c9987f1b2d586e54a1
Pawel (laptop) <pol84@live.com>
2011-07-21 11:11:03 +0200

    Revert "uzycie IsolatedStorageExplorer na PC"
    This reverts commit 8d5801eb278e063bc1cbbb90c1cd621f34444651.

--- Dietphone/App.xaml.cs
+++ Dietphone/App.xaml.cs
@@ -62,14 +62,12 @@ namespace Dietphone
         // This code will not execute when the application is reactivated
         private void Application_Launching(object sender, LaunchingEventArgs e)
         {
-            IsolatedStorageExplorer.Explorer.Start("192.168.1.100");
         }
 
         // Code to execute when the application is activated (brought to foreground)
         // This code will not execute when the application is first launched
         private void Application_Activated(object sender, ActivatedEventArgs e)
         {
-            IsolatedStorageExplorer.Explorer.RestoreFromTombstone();
         }
 
         // Code to execute when the application is deactivated (sent to background)
