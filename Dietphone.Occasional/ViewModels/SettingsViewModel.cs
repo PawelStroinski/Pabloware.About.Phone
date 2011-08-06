c7ebc9014a62334037761e13099266d5c1e9917c
Pawel (laptop) <pol84@live.com>
2011-08-06 11:09:03 +0200

    regionalne w ustawieniach

---
+++ Dietphone.Occasional/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using Dietphone.Models;
+
+namespace Dietphone.ViewModels
+{
+    public class SettingsViewModel : ViewModelBase
+    {
+        public List<string> Languages { get; private set; }
+        public List<string> ProductLocalisations { get; private set; }
+        private readonly Factories factories;
+
+        public SettingsViewModel(Factories factories)
+        {
+            this.factories = factories;
+            Languages = new List<string>();
+            ProductLocalisations = new List<string>();
+            Languages.Add("polski");
+            Languages.Add("angielski");
+            ProductLocalisations.Add("polski (Polska)");
+            ProductLocalisations.Add("angielski (Stany Zjednoczone)");
+        }
+    }
+}
--- Dietphone.Occasional/Views/Settings.xaml.cs
+++ Dietphone.Occasional/Views/Settings.xaml.cs
@@ -10,14 +10,19 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Dietphone.ViewModels;
 
 namespace Dietphone.Views
 {
     public partial class Settings : PhoneApplicationPage
     {
+        private SettingsViewModel viewModel;
+
         public Settings()
         {
             InitializeComponent();
+            viewModel = new SettingsViewModel(MyApp.Factories);
+            DataContext = viewModel;
         }
     }
 }
